d10931d932ea9e87f6e701dadc1a0c8cbb48f767
Eduardo Pelorca <pelorca@gmail.com>
2017-11-24 16:11:58 -0200

    Initial Commit

---
+++ MyAppTeste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAppTeste
+//
+//  Created by Aloc SP08161 on 24/11/2017.
+//  Copyright © 2017 Scopus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
